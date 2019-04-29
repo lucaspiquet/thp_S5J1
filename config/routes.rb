fa765faf0f266055762ecff1490c29bda843cfad
lucaspiquet <piquet.lucas@gmail.com>
2019-04-29 17:29:49 +0200

    css + bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/home'
+
+  get '/team', to: 'static_pages#team'
+  get '/contact', to: 'static_pages#contact'
 end
